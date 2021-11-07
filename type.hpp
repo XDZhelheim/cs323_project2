f6d3e3e459f2615d9bd2be14b683def7cb44fb40
MasterWanna <2467254465@qq.com>
2021-11-07 22:28:09 +0800

    implement type

--- type.hpp
+++ type.hpp
@@ -1,43 +1,57 @@
 #ifndef TYPE_HPP
 #define TYPE_HPP
 
-enum Category {
+enum Category
+{
     PRIMITIVE,
     ARRAY,
     STRUCTURE
 };
 
-enum Primitive {
+enum Primitive
+{
     INT,
     FLOAT,
     CHAR
 };
 
-class Type {
+class Type
+{
 public:
     string name;
     Category category;
-    union {
+    union
+    {
         Primitive primitive;
-        Array* array;
+        Array *array;
         vector<FieldList> structure;
     };
 };
 
-class Array {
+class Array
+{
 public:
-    Type* base;
+    Type *base;
     int size;
 
-    Array(Type* base, int size);
+    Array(Type *_base, int _size)
+    {
+        base = _base;
+        size = _size;
+    }
 };
 
-class FieldList {
+class FieldList
+{
 public:
     string name;
-    Type* type;
+    Type *type;
 
-    FieldList(string name, Type *type);
+    FieldList(string _name, Type *_type)
+    {
+        name = _name;
+        type = _type;
+    }
 };
 
 #endif
